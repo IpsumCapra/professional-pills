5626f9934fe575d84d571e73dc63c422cb306ee0
IpsumCapra <samuelcornelisse@gmail.com>
2021-03-25 11:48:14 +0100

    Added layout language file.

---
+++ resources/lang/en/layout.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    // Metadata
+    'description' => 'The brand new RegattaTracker deluxe tracking system',
+
+    // Layout header
+    'header.admin.home' => 'Admin',
+    'header.admin.users' => 'Users',
+    'header.settings' => 'Settings',
+    'header.logout' => 'Logout',
+    'header.login' => 'Login',
+    'header.register' => 'Register',
+
+    // Layout footer
+    'footer.authors' => 'Made by Sam Cornelisse for Networking',
+    'footer.source' => 'For the source code go to <a href="https://github.com/IpsumCapra/professional-pills" target="_blank" rel="noreferrer">GitHub</a>',
+];
