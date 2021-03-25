b39769943997cc3c5dfa511270bb67ab32eeacde
IpsumCapra <samuelcornelisse@gmail.com>
2021-03-25 12:23:45 +0100

    Started on user authentication.

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,24 @@ use Illuminate\Support\Facades\Route;
 
 // Home page
 Route::view('/', 'home')->name('home');
+
+// Normal routes
+Route::middleware('auth')->group(function () {
+
+});
+
+// Admin routes
+Route::middleware('admin')->group(function () {
+
+});
+
+// Guest routes
+Route::middleware('guest')->group(function () {
+    // Auth login
+    Route::view('/auth/login', 'auth.login')->name('auth.login');
+    Route::post('/auth/login', [AuthController::class, 'login']);
+
+    // Auth register
+    Route::view('/auth/register', 'auth.register')->name('auth.register');
+    Route::post('/auth/register', [AuthController::class, 'register']);
+});
