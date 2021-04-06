7dd073f0207a43f86074a7120726eaae0ffe2bff
IpsumCapra <samuelcornelisse@gmail.com>
2021-04-06 23:13:37 +0200

    Reroute x509 thru web.

--- app/Http/Middleware/ClientCertificate.php
+++ app/Http/Middleware/ClientCertificate.php
@@ -2,11 +2,39 @@
 
 namespace App\Http\Middleware;
 
+use Closure;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Http\Request;
 
 class ClientCertificate
 {
-    public function handle($request, $next, $guard = null)
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @param  null|string $guard
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next, $guard = null)
     {
-        return $next($request);
+        return $request->server('SSL_CLIENT_VERIFY');
+
+        if ($this->auth->guard($guard)->check()) {
+            return $next($request);
+        }
+
+        if (! $request->secure()) {
+            return abort(400, 'The Client Certificate auth requires a HTTPS connection.');
+        }
+
+        // If the certificate is valid, log in and remember the user.
+        if ($request->server('SSL_CLIENT_VERIFY') === 'SUCCESS') {
+            $this->auth->guard($guard)->login(static::getUserFromCert($request), true);
+
+            return $next($request);
+        }
+
+        throw new AuthenticationException('Unauthenticated.');
     }
 }
