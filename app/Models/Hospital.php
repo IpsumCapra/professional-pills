feef13ec4ce8882b1795d7e60000d90a5ca73940
IpsumCapra <samuelcornelisse@gmail.com>
2021-04-06 16:12:57 +0200

    Fixed auto adding of patients to hospitals.

--- app/Models/Hospital.php
+++ app/Models/Hospital.php
@@ -16,7 +16,7 @@ class Hospital extends Model
 
     // A hospital belongs to many users
     public function users() {
-        return $this->belongsToMany(User::class, 'hospital_patients')->withTimestamps();
+        return $this->belongsToMany(User::class, 'hospital_patients', 'patient_id')->withTimestamps();
     }
 
     // Search by a query
