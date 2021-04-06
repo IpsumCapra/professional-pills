0f3fa15f52f1e60705149a9be8dafa2b1fdcc38b
IpsumCapra <samuelcornelisse@gmail.com>
2021-04-06 23:00:21 +0200

    Reroute x509 thru web.

--- app/Http/Middleware/ClientCertificate.php
+++ app/Http/Middleware/ClientCertificate.php
@@ -2,57 +2,11 @@
 
 namespace App\Http\Middleware;
 
-use Closure;
-use Illuminate\Auth\AuthenticationException;
-use Illuminate\Http\Request;
 
 class ClientCertificate
 {
-    /**
-     * The authentication factory instance.
-     *
-     * @var \Illuminate\Contracts\Auth\Factory
-     */
-    protected $auth;
-
-    /**
-     * Create a new middleware instance.
-     *
-     * @param  \Illuminate\Contracts\Auth\Factory  $auth
-     * @return void
-     */
-    public function __construct(Auth $auth)
-    {
-        $this->auth = $auth;
-    }
-
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @param  null|string $guard
-     * @return mixed
-     */
-    public function handle(Request $request, Closure $next, $guard = null)
+    public function handle($request, $next, $guard = null)
     {
         return $next($request);
-
-        if ($this->auth->guard($guard)->check()) {
-            return $next($request);
-        }
-
-        if (! $request->secure()) {
-            return abort(400, 'The Client Certificate auth requires a HTTPS connection.');
-        }
-
-        // If the certificate is valid, log in and remember the user.
-        if ($request->server('SSL_CLIENT_VERIFY') === 'SUCCESS') {
-            $this->auth->guard($guard)->login(static::getUserFromCert($request), true);
-
-            return $next($request);
-        }
-
-        throw new AuthenticationException('Unauthenticated.');
     }
 }
