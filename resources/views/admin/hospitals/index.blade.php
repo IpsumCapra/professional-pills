f3ec20d74c20809717604efebb12916c14adaf12
IpsumCapra <samuelcornelisse@gmail.com>
2021-04-06 14:01:57 +0200

    Fixed hospital index form

--- resources/views/admin/hospitals/index.blade.php
+++ resources/views/admin/hospitals/index.blade.php
@@ -1,66 +1,54 @@
 @extends('layout')
 
-@section('title', __('admin/users.index.title'))
+@section('title', __('admin/hospitals.index.title'))
 
 @section('content')
     <div class="breadcrumb">
         <ul>
             <li><a href="{{ route('home') }}">{{ config('app.name') }}</a></li>
             <li><a href="{{ route('admin.home') }}">@lang('admin/home.breadcrumb')</a></li>
-            <li class="is-active"><a href="{{ route('admin.users.index') }}">@lang('admin/users.index.breadcrumb')</a></li>
+            <li class="is-active"><a href="{{ route('admin.hospitals.index') }}">@lang('admin/hospitals.index.breadcrumb')</a></li>
         </ul>
     </div>
 
     <div class="content">
-        <h1 class="title">@lang('admin/users.index.header')</h1>
+        <h1 class="title">@lang('admin/hospitals.index.header')</h1>
 
         <div class="columns">
             <div class="column">
                 <div class="buttons">
-                    <a class="button is-link" href="{{ route('admin.users.create') }}">@lang('admin/users.index.create')</a>
+                    <a class="button is-link" href="{{ route('admin.hospitals.create') }}">@lang('admin/hospitals.index.create')</a>
                 </div>
             </div>
 
             <form class="column" method="GET">
                 <div class="field has-addons">
                     <div class="control" style="width: 100%;">
-                        <input class="input" type="text" id="q" name="q" placeholder="@lang('admin/users.index.search_field')" value="{{ request('q') }}">
+                        <input class="input" type="text" id="q" name="q" placeholder="@lang('admin/hospitals.index.search_field')" value="{{ request('q') }}">
                     </div>
                     <div class="control">
-                        <button class="button is-link" type="submit">@lang('admin/users.index.search_button')</button>
+                        <button class="button is-link" type="submit">@lang('admin/hospitals.index.search_button')</button>
                     </div>
                 </div>
             </form>
         </div>
 
-        @if ($users->count() > 0)
-            {{ $users->links() }}
+        @if ($hospitals->count() > 0)
+            {{ $hospitals->links() }}
 
             <div class="columns is-multiline">
-                @foreach ($users as $user)
+                @foreach ($hospitals as $hospital)
                     <div class="column is-one-third">
                         <div class="box content" style="height: 100%">
                             <h2 class="title is-4">
-                                <a href="{{ route('admin.users.show', $user) }}">{{ $user->name() }}</a>
-
-                                @if ($user->role == App\Models\User::ROLE_NORMAL)
-                                    <span class="tag is-pulled-right is-success">@lang('admin/users.index.role_normal')</span>
-                                @endif
-
-                                @if ($user->role == App\Models\User::ROLE_ADMIN)
-                                    <span class="tag is-pulled-right is-danger">@lang('admin/users.index.role_admin')</span>
-                                @endif
+                                <a href="{{ route('admin.hospitals.show', $hospital) }}">{{ $hospital->name }}</a>
                             </h2>
-
-                            <p><a class="tag" href="mailto:{{ $user->email }}">{{ $user->email }}</a></p>
                         </div>
                     </div>
                 @endforeach
             </div>
-
-            {{ $users->links() }}
         @else
-            <p><i>@lang('admin/users.index.empty')</i></p>
+            <p><i>@lang('admin/hospitals.index.empty')</i></p>
         @endif
     </div>
 @endsection
--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -1,58 +1,254 @@
 @extends('layout')
 
-@section('title', __('admin/hospitals.create.title'))
+@section('title', __('admin/users.create.title'))
 
 @section('content')
     <div class="breadcrumb">
         <ul>
             <li><a href="{{ route('home') }}">{{ config('app.name') }}</a></li>
             <li><a href="{{ route('admin.home') }}">@lang('admin/home.breadcrumb')</a></li>
-            <li><a href="{{ route('admin.hospitals.index') }}">@lang('admin/hospitals.index.breadcrumb')</a></li>
-            <li class="is-active"><a
-                    href="{{ route('admin.hospitals.create') }}">@lang('admin/hospitals.create.breadcrumb')</a></li>
+            <li><a href="{{ route('admin.users.index') }}">@lang('admin/users.index.breadcrumb')</a></li>
+            <li class="is-active"><a href="{{ route('admin.users.create') }}">@lang('admin/users.create.breadcrumb')</a></li>
         </ul>
     </div>
 
-    <h1 class="title">@lang('admin/hospitals.create.header')</h1>
+    <h1 class="title">@lang('admin/users.create.header')</h1>
 
-    <form method="POST" action="{{ route('admin.hospitals.store') }}">
+    <form method="POST" action="{{ route('admin.users.store') }}">
         @csrf
 
-        <div class="field">
-            <label class="label" for="name">@lang('admin/hospitals.create.name')</label>
 
-            <div class="control">
-                <input class="input @error('name') is-danger @enderror" type="text" id="name" name="name"
-                       value="{{ old('name') }}" autofocus required>
-            </div>
+        <div class="columns">
+            <div class="column">
+                <div class="field">
+                    <label class="label" for="firstname">@lang('admin/users.create.firstname')</label>
 
-            @error('name')
-            <p class="help is-danger">{{ $errors->first('name') }}</p>
-            @enderror
-        </div>
+                    <div class="control">
+                        <input class="input @error('firstname') is-danger @enderror" type="text" id="firstname" name="firstname" value="{{ old('firstname') }}" autofocus required>
+                    </div>
 
-        <div class="field">
-            <label class="label" for="province">@lang('admin/hospitals.create.province')</label>
-
-            <div class="control">
-                <div class="select is-fullwidth @error('province') is-danger @enderror">
-                    <select id="province" name="province" required>
-                        @foreach (\App\Models\User::PROVINCES as $province)
-                            <option
-                                {{ $province == old('province', 'Drenthe') ? 'selected' : '' }} value="{{ $province }}">{{ $province }}</option>
-                        @endforeach
-                    </select>
+                    @error('firstname')
+                        <p class="help is-danger">{{ $errors->first('firstname') }}</p>
+                    @enderror
                 </div>
             </div>
 
-            @error('province')
-            <p class="help is-danger">{{ $errors->first('province') }}</p>
-            @enderror
+            <div class="column">
+                <div class="field">
+                    <label class="label" for="insertion">@lang('admin/users.create.insertion')</label>
+
+                    <div class="control">
+                        <input class="input @error('insertion') is-danger @enderror" type="text" id="insertion" name="insertion" value="{{ old('insertion') }}">
+                    </div>
+
+                    @error('insertion')
+                        <p class="help is-danger">{{ $errors->first('insertion') }}</p>
+                    @enderror
+                </div>
+            </div>
+
+            <div class="column">
+                <div class="field">
+                    <label class="label" for="lastname">@lang('admin/users.create.lastname')</label>
+
+                    <div class="control">
+                        <input class="input @error('lastname') is-danger @enderror" type="text" id="lastname" name="lastname" value="{{ old('lastname') }}" required>
+                    </div>
+
+                    @error('lastname')
+                        <p class="help is-danger">{{ $errors->first('lastname') }}</p>
+                    @enderror
+                </div>
+            </div>
+        </div>
+
+        <div class="columns">
+            <div class="column">
+                <div class="field">
+                    <label class="label" for="gender">@lang('admin/users.create.gender')</label>
+
+                    <div class="control">
+                        <div class="select is-fullwidth @error('gender') is-danger @enderror">
+                            <select id="gender" name="gender" required>
+                                <option value="{{ App\Models\User::GENDER_MALE }}" @if (App\Models\User::GENDER_MALE == old('gender', App\Models\User::GENDER_MALE)) selected @endif>
+                                    @lang('admin/users.create.gender_male')
+                                </option>
+
+                                <option value="{{ App\Models\User::GENDER_FEMALE }}" @if (App\Models\User::GENDER_FEMALE == old('gender', App\Models\User::GENDER_MALE)) selected @endif>
+                                    @lang('admin/users.create.gender_female')
+                                </option>
+
+                                <option value="{{ App\Models\User::GENDER_OTHER }}" @if (App\Models\User::GENDER_OTHER == old('gender', App\Models\User::GENDER_MALE)) selected @endif>
+                                    @lang('admin/users.create.gender_other')
+                                </option>
+                            </select>
+                        </div>
+                    </div>
+
+                    @error('gender')
+                        <p class="help is-danger">{{ $errors->first('gender') }}</p>
+                    @enderror
+                </div>
+            </div>
+
+            <div class="column">
+                <div class="field">
+                    <label class="label" for="birthday">@lang('admin/users.create.birthday')</label>
+
+                    <div class="control">
+                        <input class="input @error('birthday') is-danger @enderror" type="date" id="birthday" name="birthday" value="{{ old('birthday') }}" required>
+                    </div>
+
+                    @error('birthday')
+                        <p class="help is-danger">{{ $errors->first('birthday') }}</p>
+                    @enderror
+                </div>
+            </div>
+        </div>
+
+        <div class="columns">
+            <div class="column">
+                <div class="field">
+                    <label class="label" for="email">@lang('admin/users.create.email')</label>
+
+                    <div class="control">
+                        <input class="input @error('email') is-danger @enderror" type="email" id="email" name="email" value="{{ old('email') }}" required>
+                    </div>
+
+                    @error('email')
+                        <p class="help is-danger">{{ $errors->first('email') }}</p>
+                    @enderror
+                </div>
+            </div>
+
+            <div class="column">
+                <div class="field">
+                    <label class="label" for="phone">@lang('admin/users.create.phone')</label>
+
+                    <div class="control">
+                        <input class="input @error('phone') is-danger @enderror" type="tel" id="phone" name="phone" value="{{ old('phone') }}">
+                    </div>
+
+                    @error('phone')
+                        <p class="help is-danger">{{ $errors->first('phone') }}</p>
+                    @enderror
+                </div>
+            </div>
+        </div>
+
+        <div class="columns">
+            <div class="column">
+                <div class="field">
+                    <label class="label" for="address">@lang('admin/users.create.address')</label>
+
+                    <div class="control">
+                        <input class="input @error('address') is-danger @enderror" type="text" id="address" name="address" value="{{ old('address') }}" required>
+                    </div>
+
+                    @error('address')
+                        <p class="help is-danger">{{ $errors->first('address') }}</p>
+                    @enderror
+                </div>
+            </div>
+
+            <div class="column">
+                <div class="field">
+                    <label class="label" for="postcode">@lang('admin/users.create.postcode')</label>
+
+                    <div class="control">
+                        <input class="input @error('postcode') is-danger @enderror" type="text" id="postcode" name="postcode" value="{{ old('postcode') }}" required>
+                    </div>
+
+                    @error('postcode')
+                        <p class="help is-danger">{{ $errors->first('postcode') }}</p>
+                    @enderror
+                </div>
+            </div>
+        </div>
+
+        <div class="columns">
+            <div class="column">
+                <div class="field">
+                    <label class="label" for="city">@lang('admin/users.create.city')</label>
+
+                    <div class="control">
+                        <input class="input @error('city') is-danger @enderror" type="text" id="city" name="city" value="{{ old('city') }}" required>
+                    </div>
+
+                    @error('city')
+                        <p class="help is-danger">{{ $errors->first('city') }}</p>
+                    @enderror
+                </div>
+            </div>
+
+            <div class="column">
+                <div class="field">
+                    <label class="label" for="province">@lang('admin/users.create.province')</label>
+
+                    <div class="control">
+                        <div class="select is-fullwidth @error('province') is-danger @enderror">
+                            <select id="province" name="province" required>
+                                @foreach (\App\Models\User::PROVINCES as $province)
+                                    <option {{ $province == old('province', 'Drenthe') ? 'selected' : '' }} value="{{ $province }}">{{ $province }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                    </div>
+
+                    @error('province')
+                        <p class="help is-danger">{{ $errors->first('province') }}</p>
+                    @enderror
+                </div>
+            </div>
+        </div>
+
+        <div class="columns">
+            <div class="column">
+                <div class="field">
+                    <label class="label" for="password">@lang('admin/users.create.password')</label>
+
+                    <div class="control">
+                        <input class="input @error('password') is-danger @enderror" type="password" id="password" name="password" required>
+                    </div>
+
+                    @error('password')
+                        <p class="help is-danger">{{ $errors->first('password') }}</p>
+                    @enderror
+                </div>
+            </div>
+
+            <div class="column">
+                <div class="field">
+                    <label class="label" for="password_confirmation">@lang('admin/users.create.password_confirmation')</label>
+
+                    <div class="control">
+                        <input class="input @error('password') is-danger @enderror" type="password" id="password_confirmation" name="password_confirmation" required>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="field">
+            <label class="label" for="role">@lang('admin/users.create.role')</label>
+
+            <div class="control">
+                <div class="select is-fullwidth @error('role') is-danger @enderror">
+                    <select id="role" name="role" required>
+                        <option value="{{ App\Models\User::ROLE_NORMAL }}" @if (App\Models\User::ROLE_NORMAL == old('role', App\Models\User::ROLE_NORMAL)) selected @endif>
+                            @lang('admin/users.create.role_normal')
+                        </option>
+
+                        <option value="{{ App\Models\User::ROLE_ADMIN }}" @if (App\Models\User::ROLE_ADMIN == old('role', App\Models\User::ROLE_NORMAL)) selected @endif>
+                            @lang('admin/users.create.role_admin')
+                        </option>
+                    </select>
+                </div>
+            </div>
         </div>
 
         <div class="field">
             <div class="control">
-                <button class="button is-link" type="submit">@lang('admin/hospitals.create.button')</button>
+                <button class="button is-link" type="submit">@lang('admin/users.create.button')</button>
             </div>
         </div>
     </form>
