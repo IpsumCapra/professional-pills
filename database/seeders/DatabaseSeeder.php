e7303f5af6b454f4942ef8f34ed53966e88b57ed
IpsumCapra <samuelcornelisse@gmail.com>
2021-04-06 15:29:01 +0200

    Altered seeder for future use.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(100)->create();
         \App\Models\Hospital::factory(20)->create();
+        \App\Models\User::factory(100)->create();
     }
 }
