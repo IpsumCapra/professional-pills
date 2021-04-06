c252825541cdde393771538c26f06558af797e97
IpsumCapra <samuelcornelisse@gmail.com>
2021-04-06 14:06:41 +0200

    Added hospitals to homepage.

--- resources/lang/en/layout.php
+++ resources/lang/en/layout.php
@@ -7,6 +7,7 @@ return [
     // Layout header
     'header.admin.home' => 'Admin',
     'header.admin.users' => 'Users',
+    'header.admin.hospitals' => 'Hospitals',
     'header.settings' => 'Settings',
     'header.logout' => 'Logout',
     'header.login' => 'Login',
