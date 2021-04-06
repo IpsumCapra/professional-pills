5fb6015c6a81e93c235a0cd5f4d9c8aa3bbcb470
IpsumCapra <samuelcornelisse@gmail.com>
2021-04-06 17:11:26 +0200

    Added functions to API Patient controller.

--- app/Http/Controllers/Api/ApiPatientController.php
+++ app/Http/Controllers/Api/ApiPatientController.php
@@ -3,9 +3,16 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class ApiPatientController extends Controller
 {
-    //
+    public function index() {
+        return User::all()->paginate(config('pagination.api.limit'));
+    }
+
+    public function show(User $user) {
+        return $user;
+    }
 }
