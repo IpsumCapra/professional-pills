073504615b7860fcede981a7b404918506218b57
IpsumCapra <samuelcornelisse@gmail.com>
2021-04-07 13:42:16 +0200

    Altered auth.

--- app/Http/Middleware/ApiRoleAuth.php
+++ app/Http/Middleware/ApiRoleAuth.php
@@ -9,14 +9,14 @@ class ApiRoleAuth
 {
     public function handle(Request $request, Closure $next, ...$abilities)
     {
-        dd([
-            'req' => $request,
-            'next' => $next,
-            'abl' => $abilities
-        ]);
+//        dd([
+//            'req' => $request,
+//            'next' => $next,
+//            'abl' => $abilities
+//        ]);
         foreach ($abilities as $ability) {
             if (!$request->user()->tokenCan($ability)) {
-                abort(400, 'Access denied');
+                abort(403);
             }
         }
 
