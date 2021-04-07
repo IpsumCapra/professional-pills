59d76c6b069a9cf974e6a3da8e06275bd2d695eb
IpsumCapra <samuelcornelisse@gmail.com>
2021-04-07 14:05:08 +0200

    Finished x509 authentication.

--- routes/api.php
+++ routes/api.php
@@ -14,21 +14,21 @@ Route::middleware('auth.api:md')->get('patients/{patient}', [ApiPatientControlle
 Route::middleware('auth.api:md')->patch('research/{entry}', [ApiResearchController::class, 'update']);
 
 // Logistic routes
-Route::get('patientlocations', [ApiPatientLocationController::class, 'index']);
-Route::get('patientlocations/{patient}', [ApiPatientLocationController::class, 'show']);
-Route::get('hospitals', [ApiHospitalController::class, 'index']);
-Route::get('hospitals/{hospital}', [ApiHospitalController::class, 'show']);
-Route::post('patientlocations/{patient}', [ApiPatientLocationController::class, 'store']);
-Route::get('deliveries', [ApiDeliveryController::class, 'index']);
-Route::get('deliveries/{delivery}', [ApiDeliveryController::class, 'show']);
-Route::post('deliveries', [ApiDeliveryController::class, 'store']);
-Route::delete('deliveries/{delivery}', [ApiDeliveryController::class, 'delete']);
+Route::middleware('auth.api:logistics')->get('patientlocations', [ApiPatientLocationController::class, 'index']);
+Route::middleware('auth.api:logistics')->get('patientlocations/{patient}', [ApiPatientLocationController::class, 'show']);
+Route::middleware('auth.api:logistics')->get('hospitals', [ApiHospitalController::class, 'index']);
+Route::middleware('auth.api:logistics')->get('hospitals/{hospital}', [ApiHospitalController::class, 'show']);
+Route::middleware('auth.api:logistics')->post('patientlocations/{patient}', [ApiPatientLocationController::class, 'store']);
+Route::middleware('auth.api:logistics')->get('deliveries', [ApiDeliveryController::class, 'index']);
+Route::middleware('auth.api:logistics')->get('deliveries/{delivery}', [ApiDeliveryController::class, 'show']);
+Route::middleware('auth.api:logistics')->post('deliveries', [ApiDeliveryController::class, 'store']);
+Route::middleware('auth.api:logistics')->delete('deliveries/{delivery}', [ApiDeliveryController::class, 'delete']);
 
 // R&D routes
-Route::get('research', [ApiResearchController::class, 'index']);
-Route::get('research/placebo', [ApiResearchController::class, 'index_placebo']);
-Route::get('research/live', [ApiResearchController::class, 'index_live']);
-Route::get('research/{entry}', [ApiResearchController::class, 'show']);
-Route::post('research', [ApiResearchController::class, 'store']);
+Route::middleware('auth.api:rnd')->get('research', [ApiResearchController::class, 'index']);
+Route::middleware('auth.api:rnd')->get('research/placebo', [ApiResearchController::class, 'index_placebo']);
+Route::middleware('auth.api:rnd')->get('research/live', [ApiResearchController::class, 'index_live']);
+Route::middleware('auth.api:rnd')->get('research/{entry}', [ApiResearchController::class, 'show']);
+Route::middleware('auth.api:rnd')->post('research', [ApiResearchController::class, 'store']);
 
 // IT -- Access to all of the above.
