5c60efe9c5876897c124cf839680bf800d343090
IpsumCapra <samuelcornelisse@gmail.com>
2021-04-07 14:48:09 +0200

    Test new homepage.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,34 +6,33 @@
     <div class="content">
         @auth
             <h1 class="title">@lang('home.header_auth', ['user.firstname' => Auth::user()->firstname])</h1>
-        {{dd(['auth' => Auth::user()->hospitals()->first()])}}
-{{--            @if(Auth::user()->hospitals()->count() > 0)--}}
-{{--                <div class="box content">--}}
-{{--                    <h1 class="title">@lang('home.hospital')</h1>--}}
-{{--                    <p>{{Auth::user()->hospitals()->first()->name}}</p>--}}
-{{--                </div>--}}
-{{--            @endif--}}
-{{--            @if(Auth::user()->trials()->count() > 0)--}}
-{{--                <div class="box content">--}}
-{{--                    <h1 class="title">@lang('home.trials')</h1>--}}
-{{--                    @foreach(Auth::user()->trials()->toArray() as $trial)--}}
-{{--                        <div class="box content">--}}
-{{--                            <h1 class="title is-spaced is-4">@lang('home.trials.name', ['trial.id' => $trial->id])</h1>--}}
-{{--                            @if ($trial->success === true)--}}
-{{--                                <span class="tag is-pulled-right is-success">@lang('home.trials.success')</span>--}}
-{{--                            @endif--}}
+            @if(Auth::user()->hospitals()->count() > 0)
+                <div class="box content">
+                    <h1 class="title">@lang('home.hospital')</h1>
+                    <p>{{Auth::user()->hospitals()->first()->name}}</p>
+                </div>
+            @endif
+            @if(Auth::user()->trials()->count() > 0)
+                <div class="box content">
+                    <h1 class="title">@lang('home.trials')</h1>
+                    @foreach(Auth::user()->trials()->toArray() as $trial)
+                        <div class="box content">
+                            <h1 class="title is-spaced is-4">@lang('home.trials.name', ['trial.id' => $trial->id])</h1>
+                            @if ($trial->success === true)
+                                <span class="tag is-pulled-right is-success">@lang('home.trials.success')</span>
+                            @endif
 
-{{--                            @if ($trials->success === false)--}}
-{{--                                <span class="tag is-pulled-right is-danger">@lang('home.trials.failure')</span>--}}
-{{--                            @endif--}}
+                            @if ($trials->success === false)
+                                <span class="tag is-pulled-right is-danger">@lang('home.trials.failure')</span>
+                            @endif
 
-{{--                            @if ($trials->success === null)--}}
-{{--                                <span class="tag is-pulled-right is-warning">@lang('home.trials.pending')</span>--}}
-{{--                            @endif--}}
-{{--                        </div>--}}
-{{--                    @endforeach--}}
-{{--                </div>--}}
-{{--            @endif--}}
+                            @if ($trials->success === null)
+                                <span class="tag is-pulled-right is-warning">@lang('home.trials.pending')</span>
+                            @endif
+                        </div>
+                    @endforeach
+                </div>
+            @endif
         @else
             <h1 class="title">@lang('home.header_guest')</h1>
             <p>@lang('home.description')</p>
