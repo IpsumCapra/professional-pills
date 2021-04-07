c5cb6d491fb7e0645001fb5e0575c269c4d47b81
IpsumCapra <samuelcornelisse@gmail.com>
2021-04-07 12:22:19 +0200

    Added token generation

--- app/Http/Controllers/ApiAuthController.php
+++ app/Http/Controllers/ApiAuthController.php
@@ -14,7 +14,11 @@ class ApiAuthController extends Controller
     // Login route
     public function login(Request $request)
     {
-        dd($request);
+        $division = $request->server('REDIRECT_SSL_CLIENT_S_DN_CN');
+
+        $token = $request->user()->createToken('auth_token');
+
+        return ['token' => $token->plainTextToken];
 
         // When not successfull go back with error
         return back()->withInput()->with('error', __('auth.login.error'));
