75f5c40da83391608a45f9192696b5ddd0c3995c
IpsumCapra <samuelcornelisse@gmail.com>
2021-04-07 15:03:52 +0200

    Test new homepage.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,23 +15,22 @@
             @if(Auth::user()->trials()->count() > 0)
                 <div class="box content">
                     <h1 class="title">@lang('home.trials')</h1>
-                    {{dd(['trials' => Auth::user()->trials()->get()->toArray()])}}
-{{--                    @foreach(Auth::user()->trials()->get()->toArray() as $trial)--}}
-{{--                        <div class="box content">--}}
-{{--                            <h1 class="title is-spaced is-4">@lang('home.trials.name', ['trial.id' => $trial->id])</h1>--}}
-{{--                            @if ($trial->success === true)--}}
-{{--                                <span class="tag is-pulled-right is-success">@lang('home.trials.success')</span>--}}
-{{--                            @endif--}}
+                    @foreach(Auth::user()->trials()->get()->toArray() as $trial)
+                        <div class="box content">
+                            <h1 class="title is-spaced is-4">@lang('home.trials.name', ['trial.id' => $trial->id])</h1>
+                            @if ($trial->successful === true)
+                                <span class="tag is-pulled-right is-success">@lang('home.trials.success')</span>
+                            @endif
 
-{{--                            @if ($trials->success === false)--}}
-{{--                                <span class="tag is-pulled-right is-danger">@lang('home.trials.failure')</span>--}}
-{{--                            @endif--}}
+                            @if ($trials->successful === false)
+                                <span class="tag is-pulled-right is-danger">@lang('home.trials.failure')</span>
+                            @endif
 
-{{--                            @if ($trials->success === null)--}}
-{{--                                <span class="tag is-pulled-right is-warning">@lang('home.trials.pending')</span>--}}
-{{--                            @endif--}}
-{{--                        </div>--}}
-{{--                    @endforeach--}}
+                            @if ($trials->successful === null)
+                                <span class="tag is-pulled-right is-warning">@lang('home.trials.pending')</span>
+                            @endif
+                        </div>
+                    @endforeach
                 </div>
             @endif
         @else
