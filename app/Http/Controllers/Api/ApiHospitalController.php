02b84973c2608a9ad5a216efcea43e06fa40f72f
IpsumCapra <samuelcornelisse@gmail.com>
2021-04-07 18:30:25 +0200

    Added Hospital API.

--- app/Http/Controllers/Api/ApiHospitalController.php
+++ app/Http/Controllers/Api/ApiHospitalController.php
@@ -3,9 +3,16 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Hospital;
 use Illuminate\Http\Request;
 
 class ApiHospitalController extends Controller
 {
-    //
+    public function index() {
+        return Hospital::all()->pagination(config('pagination.api.limit'));
+    }
+
+    public function show(Hospital $hospital) {
+        return $hospital;
+    }
 }
